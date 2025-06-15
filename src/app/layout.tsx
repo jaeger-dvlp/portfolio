d191467df6ad0bd291591af63b41b0ce954c8dd3
jaeger-dvlp <omer.kayalar0101@icloud.com>
2025-06-15 20:17:36 +0300

    feat: layout & fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/main/Navbar';
+
 import './globals.css';
+
+const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
 export const metadata: Metadata = {
   title: 'Hello World',
@@ -11,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
