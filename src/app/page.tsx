33e7b91237dc133cecb8ac382dd90a1a8945efad
jaeger-dvlp <omer.kayalar0101@icloud.com>
2025-06-16 06:15:44 +0300

    add projects comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import EntryBanner from './components/home/EntryBanner';
+import Projects from './components/home/Projects';
 
 export default function Home() {
   return (
     <main className="flex min-h-[200vh] flex-col items-center justify-start">
       <EntryBanner />
+      <Projects />
     </main>
   );
 }
